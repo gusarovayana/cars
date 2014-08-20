1df9c58ebd3dbca34af9cbbf09229d5956513f4e
yana <gusyana@bigmir.net>
2014-08-20 20:27:24 +0300

    first commit

---
+++ car.php
@@ -0,0 +1,12 @@
+<?php 
+class cat
+{
+	public function myau()
+	{echo "myau";
+}
+
+	public function cat2()
+	{
+		echo "go";
+	}
+}
---
+++ cars.php
@@ -0,0 +1,16 @@
+
+<?php
+require_once __DIR__."/cat.php";
+ 
+ class cat2 extends cat
+ {
+ 	public function pi()
+ 	{
+ 		echo "pi";
+ 	}
+
+ 	public function myau()
+ 	{
+ 		echo "Maoooooooo";
+ 	}
+ 	}
